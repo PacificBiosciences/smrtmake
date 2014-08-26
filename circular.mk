66f66605811b488c20f0c771c6c47030c3c70656
James Drake <jdrake@pacificbiosciences.com>
2014-08-26 12:09:44 -0700

    Update to handle multi-fasta file
    Can now pass it a fasta file with 1 or more sequences.  Will skip any
    sequences that are less than 2*$(OVLSIZE).  Prints out a message if any
    sequence looks circular.

--- circular.mk
+++ circular.mk
@@ -1,26 +1,43 @@
 FASTA ?= polished_assembly.fasta
-OVLSIZE = 20000
+OVLSIZE ?= 20000
 ODIR = circularize
+# sequence ids suitable for file names (i.e., parse out |quiver)
+FIDS := $(shell fastalength $(FASTA) | awk '($$1>2*$(OVLSIZE)){print $$2}' | sed -r 's/([^|]+).*/\1/')
 
-all :  circ
+EOFF := $(FIDS:%=$(ODIR)/%.end.offs)
+BEGS := $(FIDS:%=$(ODIR)/%.beg.fa)
+ENDS := $(FIDS:%=$(ODIR)/%.end.fa)
+FAS  := $(FIDS:%=$(ODIR)/%.fa)
+M4S  := $(FIDS:%=$(ODIR)/%.m4)
+CIRC := $(FIDS:%=%.circ)
+
+all : $(CIRC)
 
 prepare :
 	@mkdir -p $(ODIR)
 
-circ : $(ODIR)/ovl.m4
-	@awk '($$6==0 && $$9==0 && $$12==$(OVLSIZE)){print substr($$1,0,index($$1,":"))" looks circular"}' $<
+$(CIRC) : %.circ : $(ODIR)/%.m4 
+	@awk '($$6==0 && $$9==0 && $$12==$(OVLSIZE)){print "$*: looks circular"}' $<
 
-$(ODIR)/ovl.m4 : $(ODIR)/beg.fa $(ODIR)/end.fa
+$(M4S) : %.m4 : %.beg.fa %.end.fa
 	@blasr $^ -bestn 1 -m 4 -minPctIdentity 98.0 > $@ 2> /dev/null
 
-$(ODIR)/beg.fa : $(FASTA) | prepare
+$(BEGS) : %.beg.fa : %.fa
 	@fastasubseq $< 0 $(OVLSIZE) > $@
 
-$(ODIR)/end.fa : $(FASTA) $(ODIR)/end.offset | prepare
+
+$(ENDS) : %.end.fa : %.fa %.end.offs 
 	@fastasubseq $< `cat $(word 2,$^)` $(OVLSIZE) > $@
 
-$(ODIR)/end.offset : $(FASTA) | prepare
+$(EOFF) : %.end.offs : %.fa 
 	@fastalength $< | awk '{print $$1-$(OVLSIZE)}' > $@
+
+$(FAS) : $(ODIR)/%.fa : $(FASTA) % | prepare
+	@awk '{if($$1~">"){p=$$1~/$*\y/?1:0}if(p){print}}' $< > $@
+
+$(FIDS) : ;
 
 clean :
 	@rm -rf $(ODIR)
+
+#.INTERMEDIATE : $(EOFF) $(FAS) $(BEGS) $(ENDS) $(M4S)
